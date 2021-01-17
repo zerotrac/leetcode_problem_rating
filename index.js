49156455c33af9c6dc106cdbb8ef4c82c6cf5f09
zerotrac <734688610@qq.com>
2021-01-17 23:40:37 +0800

    Add Files for Github Pages

---
+++ index.js
@@ -0,0 +1,38 @@
+new gridjs.Grid({
+    columns: [{
+        name: "ID",
+    }, {
+        name: "Title",
+        formatter: (_, row) => gridjs.html(`<a href="https://leetcode.com/problems/${row.cells[2].data}" target="_blank">${row.cells[1].data}</a>`),
+        sort: {
+            enabled: false
+        }
+    }, {
+        name: "TitleSlug",
+        hidden: true
+    }, {
+        name: "Rating",
+        formatter: (cell) => Math.round(cell)
+    }],
+    server: {
+        url: "./data.json",
+        then: data => data.map(entry => [entry.ID, entry.Title, entry.TitleSlug, entry.Rating])
+    },
+    sort: true,
+    search: {
+        selector: (cell, rowIndex, cellIndex) => cellIndex === 2 ? "" : cell
+    },
+    pagination: {
+        limit: 20
+    },
+    style: {
+        table: {
+            border: '3px solid #ccc'
+        },
+        th: {
+            'background-color': 'rgba(0, 0, 0, 0.1)',
+            color: '#000',
+            'border-bottom': '3px solid #ccc',
+        }
+    }
+}).render(document.getElementById("wrapper"));
