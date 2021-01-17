4256d0ff81da25be7b8b07f9784df19e7eed5a11
zerotrac <734688610@qq.com>
2021-01-18 00:39:42 +0800

    Add Simple Responsive

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 new gridjs.Grid({
     columns: [{
         name: "ID",
+        width: "7%",
     }, {
         name: "Title",
+        // width: "60%",
         formatter: (_, row) => gridjs.html(`<a href="https://leetcode.com/problems/${row.cells[2].data}" target="_blank">${row.cells[1].data}</a>`),
         sort: {
             enabled: false
@@ -12,6 +14,7 @@ new gridjs.Grid({
         hidden: true
     }, {
         name: "Rating",
+        width: "11%",
         formatter: (cell) => Math.round(cell)
     }],
     server: {
@@ -19,6 +22,7 @@ new gridjs.Grid({
         then: data => data.map(entry => [entry.ID, entry.Title, entry.TitleSlug, entry.Rating])
     },
     sort: true,
+    autoWidth: false,
     search: {
         selector: (cell, rowIndex, cellIndex) => cellIndex === 2 ? "" : cell
     },
@@ -32,7 +36,7 @@ new gridjs.Grid({
         th: {
             'background-color': 'rgba(0, 0, 0, 0.1)',
             color: '#000',
-            'border-bottom': '3px solid #ccc',
+            'border-bottom': '3px solid #ccc'
         }
     }
 }).render(document.getElementById("wrapper"));
